02501d1f04ad52eef52127008b4fa6ab54e8d35d
Xojimurod0808 <xacker9988@gmail.com>
2021-06-29 11:40:42 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        dots: false,
+        autoplay: 10000,
+        autoplaySpeed: 5000,
+    })
+});
